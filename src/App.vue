da31b3c23f3e6174d375292f19175cb9beae5d64
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2024-07-08 12:46:56 +0300

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import MainLayout from '@/layouts/MainLayout/index.vue'
+import MainLayout from './layouts/MainLayout/MainLayout.vue'
 </script>
 
 <template>
   <MainLayout>
     <RouterView />
-  </MainLayout>  
+  </MainLayout>
 </template>
--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from 'vue'
 
-import Drawer from "./MainLayoutDrawer.vue";
-import AppBar from "./MainLayoutAppBar.vue";
+import AppBar from './MainLayoutAppBar.vue'
+import Drawer from './MainLayoutDrawer.vue'
 
 const isDrawerOpen = ref(false)
 </script>
@@ -12,13 +12,11 @@ const isDrawerOpen = ref(false)
     <AppBar @toggle-drawer="isDrawerOpen = !isDrawerOpen" />
 
     <Drawer v-model:is-open="isDrawerOpen" />
-    
+
     <v-main style="height: 100vh">
       <slot />
     </v-main>
   </v-layout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
